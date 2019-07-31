79ba3a31c49be3d9355f353e17af94fbb1f8bbfa
Kyle Truong <kyle.truong@lifefitness.com>
2019-07-31 18:11:51 +0800

    Initial project

---
+++ Haskell/Exercises.hs
@@ -0,0 +1,214 @@
+
+halve :: Eq a => [a] -> ([a],[a])
+halve x | x == [] = ([],[])
+        | False == even (length x) = (x,[])
+        | otherwise = splitAt ((length x) `div` 2) x
+
+third :: [a] -> a
+{-third [_, _, a] = a -- pattern matching
+third (_:xs)    = xs !! 1 -- head and tail
+third a         = a !! 2  -- list indexing !!
+third xs        = head (tail (tail xs))-}
+third (_:_:x:_) = x
+
+safetail :: [a] -> [a]
+safetail b | True == null b = [] -- guarded equations
+           | otherwise = tail b 
+--safetail a      = if True == null a then [] else tail a -- a conditional expression
+--safetail []     = []
+--safetail (_:xs) = xs -- pattern matching
+
+(&&) :: Bool -> Bool -> Bool
+b && c = if b == c then 
+            if True == b then True else False
+         else False
+
+andFunc :: Bool -> Bool -> Bool
+andFunc a b = if a then b else False
+
+mult :: Int -> Int -> Int -> Int
+mult = \x -> \y -> \z -> x * y * z
+
+luhnDouble :: Int -> Int
+luhnDouble a = if 9 >= (a * 2) then a * 2 else (a * 2) - 9
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn = \x -> \y -> \z -> \w -> 0 == (luhnDouble x + y + luhnDouble z + w) `mod` 10
+
+firsts :: [(a,b)] -> [a]
+firsts ps = [x | (x,_) <- ps]
+
+length' :: [a] -> [Int]
+length' xs = [1 | _ <- xs]
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+prime :: Int -> Bool
+prime n = factors n == [1,n]
+
+primes :: Int -> [Int]
+primes n = [x | x <- [1..n], prime x]
+
+find :: Eq a => a -> [(a,b)] -> [b]
+find k t = [v | (k',v) <- t, k == k']
+
+pairs :: [a] -> [(a,a)]
+pairs xs = zip xs (tail xs)
+
+sorted :: Ord a => [a] -> Bool
+sorted xs = and [x <= y | (x,y) <- pairs xs]
+
+-- positions :: Eq a => a -> [a] -> [Int]
+-- positions x xs = [i | (x',i) <- zip xs [0..], x == x']
+
+
+------ Sum of 1 square to nth square --------
+sumSquaresTo :: Int -> Int
+sumSquaresTo x = sum [n^2 | n <- [1..x]]
+
+
+----- 5.7.1 calculated the sum 1^2 + 2^2 +...+ 100^2 -------
+sumSquares :: Int
+sumSquares = sumSquaresTo 100
+
+----- 5.7.2 Grid  ---------------
+grid :: Int -> Int -> [(Int,Int)]
+grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
+
+------- 5.7.3 Square from Grid ---------
+--square :: Int -> [(Int,Int)]
+square n = [(x,y) | (x,y) <- grid n n, x /= y]
+
+------- 5.7.4 Replicate -------
+replicate' :: Int -> a -> [a]
+replicate' t v = [v | _ <- [1..t]]
+
+------- 5.7.5 Pythagorean --------
+pyths :: Int -> [(Int,Int,Int)]
+pyths x = [(a,b,c) | a <- [1..x], b <- [1..x], c <- [1..x], a^2 + b^2 == c^2]
+
+------- 5.7.6 Perfect Integer ---------
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], x == sum (tail (reverse (factors x)))]
+
+------- 5.7.7 concat application --------------
+--comb = [(x,y) | x <- [1,2], y <- [3,4]]
+comb = concat [ [ (x,y) | y <- [3,4] ] | x <- [1,2] ]
+
+------- 5.7.8 concat application --------------
+positions :: Eq a => a -> [a] -> [Int]
+positions k t = find k [(x',i) | (x',i) <- zip t [0..]]
+
+-------- 5.7.9 scalar product --------
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [x*y | (x,y) <- zip xs ys]
+
+
+(++) :: [a] -> [a] -> [a]
+[]     ++ ys = ys
+(x:xs) ++ ys = x : xs Main.++ ys
+
+
+insert :: Ord a => a -> [a] -> [a]
+insert x []     = [x]
+insert x (y:ys) | x <= y    = x : y : ys
+                | otherwise = y : insert x ys
+
+isort :: Ord a => [a] -> [a]
+isort []     = []
+isort (x:xs) = insert x (isort xs)
+
+fibo :: Int -> Int
+fibo n | 0 == n = 0
+       | 1 == n = 1
+       | otherwise = fibo (n - 1) + fibo (n - 2)
+
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort smaller Prelude.++ [x] Prelude.++ qsort bigger where
+                   smaller = [a | a <- xs, a <= x]
+                   bigger = [b | b <- xs, b > x] 
+
+even' :: Int -> Bool
+even' 0 = True
+even' n = odd' (n-1)
+
+odd' :: Int -> Bool
+odd' 0 = False
+odd' n = even' (n-1)
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = product [1..n]
+-- factorial n | 0 > n = n 
+--             | otherwise = n * factorial(n-1)
+
+-- 6.8.6.a ----
+and' :: [Bool] -> Bool
+and' (False:_) = False
+and' []        = True
+and' (x:xs)    = and' xs
+
+-- 6.8.6.b ----
+concat' :: [[a]] -> [a]
+concat' []     = []
+concat' (x:xs) =  x Prelude.++ concat' xs
+
+-- 6.8.6.c ----
+replicate'' :: Int -> a -> [a]
+replicate'' 0 _ = []
+replicate'' n x = [x] Prelude.++ replicate'' (n-1) x 
+
+-- 6.8.6.d ----
+(!!) :: [a] -> Int -> a
+(x:xs) !! n | 0 == n = x
+            | otherwise = xs Main.!! (n-1)
+
+-- 6.8.6.e ----
+elem :: Eq a => a -> [a] -> Bool
+elem _ [] = False
+elem a (x:xs) | a == x = True
+              | otherwise = Main.elem a xs
+
+-- 6.8.7 ----
+merge :: Ord a => [a] -> [a] -> [a]
+merge x [] = x
+merge [] y = y
+merge (x:xs) (y:ys) | x < y = x : (merge xs (y:ys))
+                    | otherwise = merge (y:x:xs) ys -- y : merge (x:xs) ys
+
+-- 6.8.8 --
+halve' :: [a] -> ([a],[a])
+-- halve' x = (take ((length x) `div` 2) x, drop ((length x) `div` 2) x)
+halve' x = splitAt ((length x) `div` 2) x
+
+msort :: Ord a => [a] -> [a]
+msort []  = []
+msort [x] = [x]
+msort a = merge xs ys where
+    xs = msort (fst (halve' a))
+    ys = msort (snd (halve' a))
+
+-- 6.8.9.a --
+sumr :: Num a => [a] -> a
+sumr [] = 0
+sumr (x:xs) = x + sumr xs
+
+-- 6.8.9.b --
+taker :: Integral a => a -> [b] -> [b]
+taker _ []     = []
+taker 0 (x:xs) = [x]
+taker n (x:xs) = [x] Prelude.++ taker (n-1) xs
+
+-- 6.8.9.c
+lastr :: [a] -> a
+lastr [a]    = a
+lastr (x:xs) = lastr xs 
+
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x:acc) []
+
+reverse'' :: [a] -> [a]
+reverse'' = foldr (\x acc -> x:acc) []
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,125 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func multiply(_ firstFactor :Int) -> (Int) -> Int {
+    return { secondFactor in
+        if 0 == secondFactor { return 0 }
+        return firstFactor + multiply(firstFactor)(secondFactor - 1)
+    }
+}
+
+
+func multiply2(_ factor: Int, _ factor2: Int) -> Int {
+    if 0 == factor2 { return 0 }
+    else {
+        return factor + multiply2(factor, factor2 - 1)
+    }
+}
+
+multiply(3)(4)
+multiply2(3,4)
+
+
+////////////////
+func insert<A: Comparable>(_ value: A) -> ([A]) -> [A] {
+    return { array in
+        guard false == array.isEmpty else { return [value] }
+        if value <= array.first! {
+            var newArray = array
+            newArray.insert(value, at: 0)
+            return newArray
+        }
+        else {
+            return [array.first!] + insert(value)(Array(array.dropFirst()))
+        }
+    }
+}
+
+func isort<A: Comparable>(_ array: [A]) -> [A] {
+    guard false == array.isEmpty else { return [] }
+    return insert(array.first!)(isort(Array(array.dropFirst())))
+}
+
+insert(3)([1,2,4])
+isort([3,2,1,4])
+
+func drop(_ value: Int) -> ([Int]) -> [Int] {
+    return { array in
+        guard false == array.isEmpty else { return array }
+        guard 0 < value else { return array }
+        return drop(value - 1)(Array(array.dropFirst()))
+    }
+}
+
+drop(1)([1,2,3,4,5,6])
+
+
+func fibo(_ value: Int) -> Int {
+    if 0 == value { return 0 }
+    else if 1 == value { return 1 }
+    else { return fibo(value-1) + fibo(value-2) }
+}
+
+fibo(7)
+
+
+func qsortcustom<A: Comparable>(_ array: [A]) -> [A] {
+    guard false == array.isEmpty else { return [] }
+    let pivot = array.first!
+    let smaller = array.dropFirst().filter { $0 <= pivot }
+    let bigger = array.dropFirst().filter { $0 > pivot }
+    return qsortcustom(smaller) + [pivot] + qsortcustom(bigger)
+}
+
+qsortcustom([3,5,7,2,5,9])
+
+
+func even(_ value: Int) -> Bool {
+    guard 0 != value else { return true }
+    return odd(value - 1)
+}
+
+func odd(_ value: Int) -> Bool {
+    guard 0 != value else { return false }
+    return even(value - 1)
+}
+
+even(5)
+odd(3)
+
+
+func takeWhile<A>(function: (A) -> Bool, list: Array<A>) -> Array<A> {
+    guard false == list.isEmpty else { return list }
+    if true == function(list.first!) {
+        let newList = Array(list.dropFirst())
+        return [list.first!] + takeWhile(function: function, list: newList)
+    } else {
+        return Array<A>()
+    }
+}
+
+takeWhile(function: odd, list: [1,5,3,7])
+
+[1,2,3].reduce(0, +)
+let a = [1,2,4].reduce(0) { result, element in result + element }
+print(a)
+
+let b: [Int] = [1,2,3].reduce(Array<Int>()) { acc, element in
+    var array = acc
+    array.insert(element, at: 0)
+    return array
+}
+print(b)
+
+func foldr<A, B>(_ f: @escaping (A) -> (B) -> B) -> (B) -> ([A]) -> B {
+    return { acc in
+        return { list in
+            guard let last = list.last else { return acc }
+            let remain = Array(list.dropLast())
+            return f (last) ( foldr(f)(acc)(remain) )
+        }
+    }
+}
+
+let handler = foldr(<#T##f: (A) -> (B) -> B##(A) -> (B) -> B#>)
