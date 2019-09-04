1110c6e7c4095f5e9c1358954dcfb0fe46aa4cad
Kyle Truong <kyle.truong@lifefitness.com>
2019-09-04 18:32:34 +0800

    Add side exercise

--- Haskell/SideExercises.hs
+++ Haskell/SideExercises.hs
@@ -45,3 +45,60 @@ accumualte x ys = [x + sum ys] ++ accumualte x (init ys)
 gethash :: Int -> [Int] -> HashTable
 gethash _ [] = []
 gethash x ys = [((x + sum ys), (x : ys))] ++ gethash x (init ys)
+
+-- Complete the sockMerchant function below.
+sockMerchant :: Int -> [Int] -> Int
+sockMerchant 0 _      = 0
+sockMerchant _ []     = 0
+sockMerchant n xs = if n /= length xs then 0 
+                        else sum $ map (fst) (hash xs) 
+
+count :: Int -> [Int] -> Int
+count x = length . filter (== x)
+
+rmdups :: [Int] -> [Int]
+rmdups []     = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+                          
+hash :: [Int] -> [(Int, Int)]
+hash [] = []
+hash xs = [ (pair $ count color xs, color) | color <- rmdups xs ]
+
+pair :: Int -> Int
+pair n = n `div` 2 
+
+-- Repeated String
+-- Given an integer, , find and print the number of letter a's in the first  letters of Lilah's infinite string.
+repeatedString :: String -> Integer -> Integer
+repeatedString s n = toInteger (noains + noainsremain)
+                     where 
+                        noains        = length $ filter (== 'a') s
+                        lstring       = length s
+                        evalength     = (fromIntegral n) - lstring
+                        nofullsremain = evalength `div` lstring
+                        noremainchar  = evalength - nofullsremain * lstring
+                        noainsremain  = noains * nofullsremain + (length $ filter (== 'a') $ take noremainchar s)
+
+{- Jumping on the Clouds -}
+cl :: [Int] 
+let cl = [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+
+jumpingOnClouds :: [Int] -> Int
+jumpingOnClouds = length . optimisedjumps 0 . drop 1 . steps 0 . index0bit 
+
+index0bit :: [Int] -> [Int]
+index0bit []   = []
+index0bit bits = [i | (i,b) <- zip index bits, 0 == b] where index = iterate (+ 1) 0 
+
+steps :: Int -> [Int] -> [Int]
+steps _   []   = []
+steps n (x:xs) = (x - n) : steps x xs
+
+optimisedjumps :: Int -> [Int] -> [Int]
+optimisedjumps _     [] = []
+optimisedjumps n (x:xs) = if 1 == n && 1 == x then (n + x) : optimisedjumps (head xs) (drop 1 xs)
+                          else n : optimisedjumps x xs
+
+
+
+
